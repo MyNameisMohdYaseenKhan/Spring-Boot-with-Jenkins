3c2f1003c465442aae34aa85485181210cb063ba
MyNameisMohdYaseenKhan <yaseenkhan75067786@gmail.com>
2024-05-01 23:54:53 +0530

    initial commit

---
+++ src/main/java/p1/LearnJenkinsUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJenkinsUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJenkinsUsingSpringBootApplication.class, args);
+	}
+
+}
