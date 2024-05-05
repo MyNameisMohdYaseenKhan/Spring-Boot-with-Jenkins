12a2c564dac3fd3c0f4ca10ea4770b29dec3ba3d
Mohd Yaseen Khan <167443536+MyNameisMohdYaseenKhan@users.noreply.github.com>
2024-05-05 11:12:46 +0530

    Second change by yaseen khan
    I am simply added s.o.p for the purpose of testing

--- src/main/java/p1/HomeController.java
+++ src/main/java/p1/HomeController.java
@@ -11,6 +11,7 @@ public class HomeController {
 	public String Home() {
 		System.out.println("Hello I am Spring Boot app deploy on Jenkins Server");
 		System.out.println("I am change through GitHub");
+		System.out.println("my name is yaseen khan");
 		
 		return "home";
 	}
